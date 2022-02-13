c7dd8171f8db222f12dc49a6344c5b5c8407de4c
Christina Bohn <christinajbohn@gmail.com>
2022-02-12 23:12:54 -0800

    Defined columns in Models.

--- models/Category.js
+++ models/Category.js
@@ -6,10 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
     id: {
-      autoIncrement: true,
-      DataTypes: INTEGER
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
+  },
+  {
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   },
   {
