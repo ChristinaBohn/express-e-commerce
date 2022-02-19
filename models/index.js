89b3a3535c47ebe1455d1f1394d9abd46a17b056
Christina Bohn <christinajbohn@gmail.com>
2022-02-18 22:54:40 -0800

    Set category to cascade on delete to fix Insomnia error.

--- models/index.js
+++ models/index.js
@@ -3,17 +3,17 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
+
+Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'cascade',
+  hooks: true
 });
 
-// Categories have many Products
-Category.hasMany(Product, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
   through: {
@@ -22,7 +22,6 @@ Product.belongsToMany(Tag, {
   },
 });
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   foreignKey: 'tag_id',
   through: {
